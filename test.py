961b0375405c62a8e50bce254df1b071ebe2f88e
555vedant <vedantvvk5@gmail.com>
2024-04-13 02:36:47 +0530

    added new file

---
+++ test.py
@@ -0,0 +1,7 @@
+# from flask import Flask, render_template, request, jsonify
+import pickle
+# import numpy as np
+# import sklearn
+# Load the trained model
+model = pickle.load(open('model2.pkl', 'rb'))
+
